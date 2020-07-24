80f1bab4055c1fb3c6e3d46d4e77d0b37636c67b
unva1idated <unva1idated@protonmail.com>
2020-07-24 15:17:17 +1000

    Fixed preannouncements not rescheduling after first job run, adjusted update rule in config to run every 15 minutes

--- modules/scheduler.js
+++ modules/scheduler.js
@@ -8,9 +8,10 @@ const config = require("../config.json");
 
 exports = module.exports = {}
 
+exports.scheduled_events = []
+
 exports.initAnnouncements = function(client) {
 	// Announcer events
-	let events = []
 	for (let e in config.events) {
 		let event = config.events[e]
 		// Schedule announcement
@@ -18,23 +19,33 @@ exports.initAnnouncements = function(client) {
 			event.rule, 
 			async function(){ await exports.announce(client, event, false) }
 		)
-		events[event.name] = job;
+		scheduled_events[event.name] = job;
 
-		// Get the date of the next invocation
-		let date = utils.subtractMinutes(job.nextInvocation(), 5)
 		// Schedule preannouncement
 		let jobpre = schedule.scheduleJob(
 			utils.subtractMinutes(job.nextInvocation(), 5),
 			async function(){ await exports.announce(client, event, true) }
 		)
 	}
-	return events;
+	return scheduled_events;
 }
 
 exports.announce = async function(client, event, pre) {
 	let chan = await client.channels.get(config.eventChannelId)
 	if (pre) { await chan.send(`The **${event.name}** will be open in **5 minutes.**`); return; }
 	await chan.send(`**Can now register for the ${event.name}.**`)
+	// Reschedule preannouncement
+	exports.schedulePreAnnouncement(client, event)
+}
+
+exports.schedulePreAnnouncement = function(client, event) {
+	let job = exports.scheduled_events[event.name]
+	
+	// Schedule preannouncement
+	let jobpre = schedule.scheduleJob(
+		utils.subtractMinutes(job.nextInvocation(), 5),
+		async function(){ await exports.announce(client, event, true) }
+	)
 }
 
 exports.initUpdateChecker = function(client) {
